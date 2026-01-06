469f64ec9c6ab0ec5a45a2e01febd5ae67a549ea
Akshay-hub-007 <akshaykalangi9@gmail.com>
2026-01-06 23:41:43 +0530

    Add SplashScreen and AuthScreen components with animations and biometric authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,46 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { StyleSheet, Text, View, Animated } from 'react-native'
-
+import { Ionicons } from "@expo/vector-icons"
+import { useRouter } from 'expo-router'
 
 const SplashScreen = () => {
 
     const fadeAnim = useRef(new Animated.Value(0)).current
-    const scaleAnim = useRef(new Animated.Value(0)).current
+    const scaleAnim = useRef(new Animated.Value(0.5)).current
+    const router = useRouter()
+    useEffect(() => {
+        Animated.parallel([
+            Animated.timing(fadeAnim, {
+                toValue: 1,
+                duration: 1000,
+                useNativeDriver: true
+            }),
+            Animated.spring(scaleAnim, {
+                toValue: 1,
+                tension: 10,
+                friction: 2,
+                useNativeDriver: true
+            })
+        ]).start()
+        const timer = setTimeout(() => {
+            router.replace("/auth")
+        }, 2000)
+
+        return () => clearTimeout(timer);
+    }, [])
     return (
         <View style={styles.container}>
             <Animated.View
                 style={[
                     styles.iconContainer,
-                    { 
+                    {
                         opacity: fadeAnim,
-
+                        transform: [{ scale: scaleAnim }]
                     }
                 ]}>
 
-                <Text>SplashScreen</Text>
+                <Ionicons name='medical' size={100} color={'white'} />
+                <Text style={styles.appName}>SplashScreen</Text>
             </Animated.View>
         </View>
     )
@@ -33,6 +56,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     iconContainer: {
+        alignItems: 'center'
+    },
+    appName: {
         color: 'white',
         fontSize: 32,
         fontWeight: 'bold',
